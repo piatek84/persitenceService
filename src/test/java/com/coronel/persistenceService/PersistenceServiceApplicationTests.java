17b0429413c6ca9dd23c518079eb0c2e411ec11e
Juan Coronel <36151039+piatek84@users.noreply.github.com>
2025-03-29 22:22:53 +0100

    Update PersistenceServiceApplicationTests.java

--- src/test/java/com/coronel/persistenceService/PersistenceServiceApplicationTests.java
+++ src/test/java/com/coronel/persistenceService/PersistenceServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PersistenceServiceApplicationTests {
-
+//TODO: Add testcontainers
 	@Test
 	void contextLoads() {
 	}
